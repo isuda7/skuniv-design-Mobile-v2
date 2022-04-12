cbc2c888351e386e9b5ad6006228660f532156d3
kykim011 <trovatore011@gmail.com>
2022-04-12 11:12:21 +0900

    현황판

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -61,6 +61,7 @@ import LearningOverview from './views/Learning/LearningOverview'
 import LearningOverviewTeacherIntroduce from './views/Learning/LearningOverviewTeacherIntroduce'
 import LearningOverviewTeacherLecture from './views/Learning/LearningOverviewTeacherLecture'
 import LearningContents from './views/Learning/LearningContents'
+import LearningOverviewCurriculum from './views/Learning/LearningOverviewCurriculum'
 
 // Sub
 // import Sample from './views/Sample/';
@@ -133,6 +134,7 @@ const Router = () => {
                     <Route path='/Learning/LearningOverview' element={<LearningOverview />} />
                     <Route path='/Learning/LearningOverviewTeacherIntroduce' element={<LearningOverviewTeacherIntroduce />} />
                     <Route path='/Learning/LearningOverviewTeacherLecture' element={<LearningOverviewTeacherLecture />} />
+                    <Route path='/Learning/LearningOverviewCurriculum' element={<LearningOverviewCurriculum />} />
                 </Route>
             </Routes>
         </HashRouter>
